1dff231ebcd478c1853c4cba2bfa3f1b1aa79ecc
Boris Krasnov <crasnov.boris@yandex.ru>
2023-07-20 15:31:21 +0300

    С помощью линтера исправили папку tests

--- tests/QueryBuilder/QueryBuilderTest.php
+++ tests/QueryBuilder/QueryBuilderTest.php
@@ -32,27 +32,32 @@ class QueryBuilderTest extends TestCase
 
     public function testWhere(): void
     {
-        $this->assertEquals('WHERE amount = 3 '
-            , $this->queryBuilder->where('amount','=','3')->getQuery());
+        $this->assertEquals('WHERE amount = 3 ', $this->queryBuilder->where('amount', '=', '3')->getQuery());
     }
 
     public function testWhereIn(): void
     {
-        $this->assertEquals("WHERE testvalue IN ('testvalue','anothervalue') ",
-            $this->queryBuilder->whereIn('testvalue',['testvalue','anothervalue'])->getQuery());
+        $this->assertEquals(
+            "WHERE testvalue IN ('testvalue','anothervalue') ",
+            $this->queryBuilder->whereIn('testvalue', ['testvalue','anothervalue'])->getQuery()
+        );
     }
 
     public function testOrderBy(): void
     {
-        $this->assertEquals('ORDER BY some ASC ',
-        $this->queryBuilder->orderBy('some','ASC')->getQuery());
+        $this->assertEquals(
+            'ORDER BY some ASC ',
+            $this->queryBuilder->orderBy('some', 'ASC')->getQuery()
+        );
     }
 
     public function testJoin(): void
     {
-        $this->assertEquals('LEFT JOIN tableName ON firstColumn = secondColumn ',
-        $this->queryBuilder->join('LEFT JOIN','tableName','firstColumn','secondColumn')
-        ->getQuery());
+        $this->assertEquals(
+            'LEFT JOIN tableName ON firstColumn = secondColumn ',
+            $this->queryBuilder->join('LEFT JOIN', 'tableName', 'firstColumn', 'secondColumn')
+        ->getQuery()
+        );
     }
 
     public function testlimit(): void
@@ -62,41 +67,58 @@ class QueryBuilderTest extends TestCase
 
     public function testAndOr(): void
     {
-        $this->assertEquals('AND test = something ',$this->queryBuilder->andOr('AND','test','=',
-        'something')->getQuery());
+        $this->assertEquals('AND test = something ', $this->queryBuilder->andOr(
+            'AND',
+            'test',
+            '=',
+            'something'
+        )->getQuery());
     }
 
     public function testGroupBy(): void
     {
-        $this->assertEquals('GROUP BY category ',$this->queryBuilder->groupBy('category')->getQuery());
+        $this->assertEquals('GROUP BY category ', $this->queryBuilder->groupBy('category')->getQuery());
     }
 
     public function testHaving(): void
     {
-        $this->assertEquals('HAVING SUM(a * b) > 500 ',
-            $this->queryBuilder->having('SUM','a * b','>','500')->getQuery());
+        $this->assertEquals(
+            'HAVING SUM(a * b) > 500 ',
+            $this->queryBuilder->having('SUM', 'a * b', '>', '500')->getQuery()
+        );
     }
 
     public function testCreateNewRecordQuery(): void
     {
-        $this->assertEquals("INSERT INTO tableName (columnOne, columnTwo) VALUES ('valueOne', 'valueTwo') ",
-        $this->queryBuilder->createNewRecord('tableName',['valueOne','valueTwo'],['columnOne','columnTwo'])
-        ->getQuery());
+        $this->assertEquals(
+            "INSERT INTO tableName (columnOne, columnTwo) VALUES ('valueOne', 'valueTwo') ",
+            $this->queryBuilder->createNewRecord('tableName', ['valueOne','valueTwo'], ['columnOne','columnTwo'])
+        ->getQuery()
+        );
     }
 
     public function testUpdateRecordsQuery(): void
     {
-        $this->assertEquals("UPDATE tableName SET name = Boris WHERE age = 25 ",
-        $this->queryBuilder->updateRecords('tableName','name','=','Boris',
-            ['key' =>'age',"comparisonSign" => '=','value' => 25])
-            ->getQuery());
+        $this->assertEquals(
+            "UPDATE tableName SET name = Boris WHERE age = 25 ",
+            $this->queryBuilder->updateRecords(
+                'tableName',
+                'name',
+                '=',
+                'Boris',
+                ['key' =>'age',"comparisonSign" => '=','value' => 25]
+            )
+            ->getQuery()
+        );
     }
 
     public function testDeleteRecordQuery(): void
     {
-        $this->assertEquals('DELETE FROM tableName WHERE age = 25 ',
-        $this->queryBuilder->deleteRecord('tableName',['key' =>'age',"comparisonSign" => '=','value' => 25])
-            ->getQuery());
+        $this->assertEquals(
+            'DELETE FROM tableName WHERE age = 25 ',
+            $this->queryBuilder->deleteRecord('tableName', ['key' =>'age',"comparisonSign" => '=','value' => 25])
+            ->getQuery()
+        );
     }
 
     public function testDeleteRow(): void
@@ -105,13 +127,13 @@ class QueryBuilderTest extends TestCase
         $startAmountOfRows = $this->queryBuilder->getPdo()->query('SELECT COUNT(*) FROM review')->fetchColumn();
 
         // Проверка запроса на удаление
-        $this->queryBuilder->deleteRecord('review',['key' =>'id',"comparisonSign" => '=','value' => 19])
+        $this->queryBuilder->deleteRecord('review', ['key' =>'id',"comparisonSign" => '=','value' => 19])
             ->execute();
 
         // Количество рядов после удаления
         $finishAmountOfRows = $this->queryBuilder->getPdo()->query('SELECT COUNT(*) FROM review')->fetchColumn();
 
-        $this->assertEquals($finishAmountOfRows,$startAmountOfRows - 1);
+        $this->assertEquals($finishAmountOfRows, $startAmountOfRows - 1);
     }
 
     public function testInsertRow(): void
@@ -119,29 +141,32 @@ class QueryBuilderTest extends TestCase
         $startAmountOfRows = $this->queryBuilder->getPdo()->query('SELECT COUNT(*) FROM review')->fetchColumn();
 
         //
-        $this->queryBuilder->createNewRecord('review',['test7',5,1],['body','rating','product_id'])
+        $this->queryBuilder->createNewRecord('review', ['test7',5,1], ['body','rating','product_id'])
             ->execute();
 
         $finishAmountOfRows = $this->queryBuilder->getPdo()->query('SELECT COUNT(*) FROM review')->fetchColumn();
 
-        $this->assertEquals($finishAmountOfRows,$startAmountOfRows + 1);
+        $this->assertEquals($finishAmountOfRows, $startAmountOfRows + 1);
     }
 
     public function testEditRow(): void
     {
 
-        $this->queryBuilder->updateRecords('review','rating','=',2,
-            ['key' => 'id','comparisonSign'=>'=','value' => 1])->execute();
+        $this->queryBuilder->updateRecords(
+            'review',
+            'rating',
+            '=',
+            2,
+            ['key' => 'id','comparisonSign'=>'=','value' => 1]
+        )->execute();
 
         $expectedRating = 2;
 
         $elements = $this->queryBuilder->getPdo()->query('SELECT id,rating FROM review ')->fetchAll();
 
-        foreach ($elements as $element)
-        {
-            if ($element['id'] === 1)
-            {
-                $this->assertEquals($expectedRating,$element['rating']);
+        foreach ($elements as $element) {
+            if ($element['id'] === 1) {
+                $this->assertEquals($expectedRating, $element['rating']);
             }
         }
     }
